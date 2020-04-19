f351200eb64b31abe123d663c337426ec1efe56f
Anas Mubarak <anasvp444@gmail.com>
2020-04-20 05:37:20 +0800

    m

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class HomeScreenTopPart extends StatefulWidget {
 
 class _HomeScreenTopPartState extends State<HomeScreenTopPart> {
   var selectedLocationindex = 0;
-
+  bool isFlightSelected =true;
 
   @override
   Widget build(BuildContext context) {
@@ -78,9 +78,62 @@ class _HomeScreenTopPartState extends State<HomeScreenTopPart> {
                     PopupMenuItem(
                       child:Text(locations[1],style:dropdownMenuItemStyle),value: 1 
                     ,)
-                  ])
+                  ]),
+                  Spacer(),
+                Icon(Icons.settings, color: Colors.white)
               ],),
-            )
+            ),
+            SizedBox(height:50.0),
+            Text('Where Would\n you want to go', style: TextStyle(fontSize:24.0,color: Colors.white),textAlign: TextAlign.center,),
+            SizedBox(height:30.0),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal:32.0),
+               child: Material(
+                 elevation: 5.0,
+                 borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                 child: TextField(
+                   controller:TextEditingController(text:locations[0]),
+                   style: dropdownMenuItemStyle,
+                   cursorColor: appTheme.primaryColor,
+                   decoration: InputDecoration(
+                     contentPadding: EdgeInsets.symmetric(horizontal:32.0, vertical: 14.0),
+                     suffixIcon: Material(
+                       elevation:2.0,
+                       borderRadius:BorderRadius.all(Radius.circular(30.0)),
+                       child:Icon(Icons.search)
+                     ),
+                     border:InputBorder.none
+                   ),
+                 ),
+               ),),
+                SizedBox(
+                  height: 20.0,
+                ),
+               Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                 children:[
+                   InkWell(
+                     child:
+                   ChoiseChip(Icons.flight_takeoff,"flights",isFlightSelected),
+                   onTap: (){
+                     setState((){
+                        isFlightSelected =true;
+                     });
+                   },
+                   ),
+                   SizedBox(width:20.0),
+                   InkWell(
+                     child:
+                      ChoiseChip(Icons.hotel,"Hotels",!isFlightSelected),
+                      onTap: (){setState(() {
+                             isFlightSelected =false;    
+                                            });
+                      }
+                   )
+                   
+                 ]
+               )
           ]
         ),),)
       ],      
@@ -88,6 +141,44 @@ class _HomeScreenTopPartState extends State<HomeScreenTopPart> {
   }
 }
 
+
+class ChoiseChip extends StatefulWidget {
+
+  final IconData icon;
+  final String text;
+  final bool isSelected;
+
+  ChoiseChip(this.icon,this.text ,this.isSelected);
+  @override
+  _ChoiseChipState createState() => _ChoiseChipState();
+}
+
+class _ChoiseChipState extends State<ChoiseChip> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal:10.0, vertical: 5.0),
+      decoration: widget.isSelected ?
+       BoxDecoration(
+        color: Colors.white.withOpacity(0.15),
+        borderRadius: BorderRadius.all(Radius.circular(20.0),)
+      )
+      : null,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children:[
+        Icon(widget.icon,
+             size: 20.0,
+            color: Colors.white,),
+            SizedBox(width:8.0),
+        Text(widget.text,
+         style: TextStyle(color:Colors.white, fontSize: 16.0),)
+      ]),
+    );
+    
+  }
+}
 
 
 
